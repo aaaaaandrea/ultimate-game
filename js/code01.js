496e59d5b7ed8222937fba6f5c1f906d15595354
Andrea Židkova <zidkovaandrea66@gmail.com>
2020-11-28 13:06:49 +0100

    ultimate-game 01.0.1

---
+++ js/code01.js
@@ -0,0 +1,18 @@
+document.body.style.backgroundColor = 'rgb(22, 23, 27)';
+
+document.getElementById("start1").addEventListener('click',
+    function () {
+        window.location.href = "game1.html";
+
+    })
+
+document.getElementById("start2").addEventListener('click',
+    function () {
+        window.location.href = "game2.html";
+
+    })
+document.getElementById("start3").addEventListener('click',
+    function () {
+        window.location.href = "game3.html";
+
+    })
